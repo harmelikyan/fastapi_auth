839b2ae72f3346a77e423ce42c339fc228f591a0
Gaya-G <65981745+Gaya-G@users.noreply.github.com>
2023-08-13 10:12:22 +0100

    Update user.py

--- api/v1/endpoints/user.py
+++ api/v1/endpoints/user.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.get("/me", response_model=User)
-async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+async def read_users_me(current_user: Annotated[User,  Depends(get_current_active_user)]):
     return current_user
 
 
